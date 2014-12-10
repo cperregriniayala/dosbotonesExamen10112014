6cc9b8cc780b604536df988759db2a5406f38813
cperregriniayala <cperregriniayala@danielcastelao.org>
2014-12-10 11:20:55 +0100

    para el tercer apartado.Modificamos el codigo para los botones hola ya adios

--- app/src/main/java/com/example/cperregriniayala/dosbotones/MyActivity.java
+++ app/src/main/java/com/example/cperregriniayala/dosbotones/MyActivity.java
@@ -1,8 +1,6 @@
-
 package com.example.cperregriniayala.dosbotones;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,25 +12,16 @@ public class MyActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
-        String salutation="Hola/Adios";
-        Intent intento = new Intent(MyActivity.this,MyActivity2.class);
-        intento.putExtra("Saludo",salutation);
-        startActivityForResult(intento,1);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
+               getMenuInflater().inflate(R.menu.my, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
@@ -40,3 +29,4 @@ public class MyActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 }
+
--- app/src/main/java/com/example/cperregriniayala/dosbotones/MyActivity2.java
+++ app/src/main/java/com/example/cperregriniayala/dosbotones/MyActivity2.java
@@ -1,63 +1,70 @@
 package com.example.cperregriniayala.dosbotones;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-import com.example.cperregriniayala.dosbotones.R;
+import com.example.cperregriniayala.dosbotones.MyActivity;
+
+import static com.example.cperregriniayala.dosbotones.R.id.botonH;
+
 
 public class MyActivity2 extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_activity2);
+        setContentView(R.layout.activity_my);
 
-        //me falta el ONCLICLICK
+        Button bhola=(Button)findViewById(botonH);
+        bhola.setOnClickListener(new View.OnClickListener ()
+        {
+            @Override
+            public void onClick(View v)
+            {
 
-        //Relacionamos el boton del layout con botón java.
-        Button botonh = (Button)findViewById(R.id.botonH);
-        //Lo ponemos a escuchar para cuando sea pulsado
-        botonh.setOnClickListener(this);
+                Intent intento = new Intent(MyActivity2.this,MyActivity.class);
+                Bundle reci=new Bundle();
+                reci.putString("nombre","damian");
+                intento.putExtras(reci);
+                startActivityForResult(intento, 1);
+            }
+        });
 
     }
-
-    //Aquí se irá administrando cuando un botón es pulsado
     @Override
-    public void onClick(View v) {
-        /*Comprobamos a que id pertenece el id del botón pulsado, en este caso como solo tenemos 1
-        siempre va a ser el mismo pero se puede dar el caso de que haya más de un botón, entonces aquí se controlaría que botón se ha pulsado.
-        */
-        Intent i = new Intent();
-        i.putExtra("Respuesta","Hola");
-        setResult(RESULT_OK,i);
-        finish();
-    }
-
-
-    private void setResult(int resultOk, Intent i) {
-
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my_activity2, menu);
+        getMenuInflater().inflate(R.menu.my,menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         int id = item.getItemId();
+
         if (id == R.id.action_settings) {
             return true;
         }
+
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        String res = data.getExtras().getString("respuesta");
+        Context context = getApplicationContext();
+        CharSequence text = res;
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
